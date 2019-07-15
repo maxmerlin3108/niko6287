94734d836644507ea6b761166c3f6b289c6131f8
maxh2 <Vous@exemple.com>
2019-07-15 16:35:10 +0200

    mon premier commit

---
+++ tableautest.php
@@ -0,0 +1,14 @@
+<table border="1px" width=800 height=800>
+<?php
+$ligne=10;
+$colonne=10;
+  for ($i=0;$i<$ligne; $i++){
+     echo '<tr>';
+     for ($j=0;$j<$colonne; $j++){
+      if(($j+$i)%2==0)
+echo '<td>&nbsp</td>';
+     else
+echo '<td bgcolor=black>&nbsp</td>';
+  }
+?>
+</table>
---
+++ variable01.php
@@ -0,0 +1,24 @@
+<?php
+
+
+print( "hello<br>" );
+
+
+$nom         = "Lupin";
+$prenom     = "Arsene";
+$string     = "bonjour ";
+
+
+$nom = strtoupper($nom);
+
+$resultat = $string.$prenom." ".$nom;
+
+print( $nom );
+print( "<br>"); 
+print( $resultat );
+
+
+
+
+
+?>
